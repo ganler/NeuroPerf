9ce5d612abcfbe4cad229192cfd94ab4aa69d8e7
ganler <jaway.liu@gmail.com>
2021-12-05 03:51:55 -0600

    feat: downstream dataloader

--- neuroprof/dataset.py
+++ neuroprof/dataset.py
@@ -1,29 +1,101 @@
+from os import linesep
 from torch.utils.data import Dataset
-from transformers import RobertaTokenizerFast
+from transformers import RobertaTokenizerFast, BatchEncoding
+from tokenizers import Encoding, Tokenizer
 from pathlib import Path
 import torch
+import numpy as np
 
 from multiprocessing import Pool, cpu_count
 import time
+import pickle
 
-__GLOBAL_TOKENIZER__ = RobertaTokenizerFast(
+__G_FAST_TOKENIZER__ = RobertaTokenizerFast(
     tokenizer_file='./byte-level-bpe.tokenizer.json')
-def tokenize_them_all(file_list):
+
+__G_LONG_TOKENIZER__ = Tokenizer.from_file('./byte-level-bpe.tokenizer.json')
+__G_LONG_TOKENIZER__.enable_truncation(max_length=512 * 8) # Longer range.
+
+def encode_string(string):
+    return __G_FAST_TOKENIZER__.encode_plus(
+        string, max_length=512, truncation=True, padding=True).input_ids
+
+def encode_pickle(path):
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+        
+        bottleneck_id = np.argmax([v for _, v in data.data])
+        lines = [line for line, _ in data.data]
+        
+        def get_line_range(line_id):
+            line_id = min(line_id, len(lines) - 1)
+            lrange = (0, 0)
+            for line in lines[:line_id]:
+                lrange[0] += len(line)
+            lrange[1] = lrange[0] + len(lines[line_id])
+            return lrange
+
+        ########################################################################
+        # lines[bottleneck_id] is unique in the file!
+
+        # line_start = max(0, bottleneck_id - 1)
+        # for _ in range(128):
+        #     if line_start == 0 or lines[line_start - 1] == lines[bottleneck_id]:
+        #         break
+        #     line_start -= 1
+        
+        # line_end = min(len(lines) - 1, bottleneck_id + 1)
+        # for _ in range(128):
+        #     if line_end == len(lines) - 1 or lines[line_end] == lines[bottleneck_id]:
+        #         break
+        #     line_end += 1
+        ########################################################################
+        
+        line_start = max(0, bottleneck_id - 128)
+        line_end = min(len(lines) - 1, bottleneck_id + 128)
+
+        original_bottleneck_range = get_line_range(bottleneck_id)
+        lower_lim = get_line_range(bottleneck_id - 128)[0]
+        upper_lim = get_line_range(bottleneck_id + 128)[1]
+        offset_bottleneck_range = (
+            original_bottleneck_range[0] - lower_lim, 
+            original_bottleneck_range[1] - upper_lim)
+
+        cache = __G_LONG_TOKENIZER__.encode_plus(
+            '\n'.join(lines[line_start:line_end]))
+
+        if cache.offsets[-1] >= offset_bottleneck_range[1]:
+            return BatchEncoding(cache.truncate(512)), bottleneck_id - line_start
+        else:
+            bottleneck_token_id_end = None
+            for i, offset in enumerate(cache.offsets):
+                if offset[1] >= offset_bottleneck_range[1]:
+                    bottleneck_token_id_end = i
+                    break
+            cache_valid_start = max(0, bottleneck_token_id_end - 512)
+            return BatchEncoding(Encoding(cache.ids[cache_valid_start:(cache_valid_start + 512)])), bottleneck_id - line_start
+
+
+def encode_text(path):
+    return encode_string(Path(path).read_text())
+
+def tokenize_them_texts(file_list):
     ret = []
     for path in file_list:
-        ret.append(__GLOBAL_TOKENIZER__.encode_plus(Path(path).read_text(), max_length=512, truncation=True, padding=True).input_ids)
+        ret.append(encode_text(path))
     return ret
 
 class X86PerfFineTuneDataset(Dataset):
-    def __init__(self, evaluate: bool = False):
+    def __init__(self, evaluate: bool = False, train_percent = 0.9, pretraining = True): # TODO: Pretraining...
         # Create the tokenizer from a trained one
         self.examples = []
 
         files = [str(x) for x in Path("./DATASET/text/").glob("**/*.txt")]
+        n_eval = int(len(files) * (1 - train_percent))
         if evaluate:
-            files = files[:len(files) // 10]
+            files = files[:n_eval]
         else:
-            files = files[len(files) // 10:]
+            files = files[n_eval:]
 
         
         t0 = time.time()
@@ -32,8 +104,7 @@ class X86PerfFineTuneDataset(Dataset):
         if n_process <= 1:
             print('... loading dataset :: [single-threads]')
             for path in files:
-                self.examples.append(
-                    __GLOBAL_TOKENIZER__.encode_plus(Path(path).read_text(), max_length=512, truncation=True, padding=True).input_ids)
+                self.examples.append(encode_text(path))
         else:
             print(f'... loading dataset :: [{n_process}-threads]')
             size = (len(files) + n_process - 1) // n_process
@@ -41,7 +112,7 @@ class X86PerfFineTuneDataset(Dataset):
                 task_list = []
                 for i in range(n_process):
                     task_list.append(files[i*size:(i+1)*size])
-                for v in p.map(tokenize_them_all, task_list):
+                for v in p.map(tokenize_them_texts, task_list):
                     self.examples += v
         data_loading_time = time.time() - t0
         print(f'... finished in {data_loading_time}s --- {len(files) / data_loading_time} files / s')
--- neuroprof/downstream.py
+++ neuroprof/downstream.py
@@ -1,8 +1,6 @@
 from transformers import RobertaConfig
 from transformers import RobertaForMaskedLM
-from transformers.utils.dummy_pt_objects import RobertaModel
 from neuroprof.dataset import X86PerfFineTuneDataset
-import torch
 
 if __name__ == '__main__':
     import argparse
@@ -28,8 +26,5 @@ if __name__ == '__main__':
     tokenizer = RobertaTokenizerFast(
         tokenizer_file='./byte-level-bpe.tokenizer.json')
 
-    train_dataset = X86PerfFineTuneDataset(evaluate=False)
-    eval_dataset = X86PerfFineTuneDataset(evaluate=True)
-
-    # checkpoint = torch.load(args.model, map_location='cuda:0')
-    # model.load_state_dict(checkpoint['model_state_dict'])
+    train_dataset = X86PerfFineTuneDataset(evaluate=False, pretraining=False)
+    eval_dataset = X86PerfFineTuneDataset(evaluate=True, pretraining=False)
