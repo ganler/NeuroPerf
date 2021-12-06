7aa3a6bcbbcb6c63eb9e8f470ec53fe44b2e28b0
ganler <jaway.liu@gmail.com>
2021-12-06 03:54:38 -0600

    feat: downstream training finished

--- neuroprof/dataset.py
+++ neuroprof/dataset.py
@@ -1,7 +1,8 @@
-from os import linesep
+from neuroprof.data_gen import PerfData
+
 from torch.utils.data import Dataset
-from transformers import RobertaTokenizerFast, BatchEncoding
-from tokenizers import Encoding, Tokenizer
+from transformers import RobertaTokenizerFast
+from tokenizers import Tokenizer
 from pathlib import Path
 import torch
 import numpy as np
@@ -14,26 +15,43 @@ __G_FAST_TOKENIZER__ = RobertaTokenizerFast(
     tokenizer_file='./byte-level-bpe.tokenizer.json')
 
 __G_LONG_TOKENIZER__ = Tokenizer.from_file('./byte-level-bpe.tokenizer.json')
-__G_LONG_TOKENIZER__.enable_truncation(max_length=512 * 8) # Longer range.
+__G_LONG_TOKENIZER__.enable_truncation(max_length=512 * 8)  # Longer range.
+
 
 def encode_string(string):
     return __G_FAST_TOKENIZER__.encode_plus(
-        string, max_length=512, truncation=True, padding=True).input_ids
+        string, max_length=512, truncation=True, padding='max_length').input_ids
+
+
+def padding(ids, labels):
+    attention_mask = torch.ones(512, dtype=torch.long)
+    attention_mask[ids.shape[0]:] = 0  # unmask.
+    if ids.shape[0] < 512:
+        ids = torch.cat(
+            [ids, torch.ones(512 - ids.shape[0], dtype=torch.long) * 2])
+        # 2 means padding.
+        labels = torch.cat([labels, torch.zeros(
+            512 - labels.shape[0], dtype=torch.float)])
+    assert ids.shape[0] == 512, ids.shape
+    assert labels.shape[0] == 512, labels.shape
+    return ids, attention_mask, labels
+
 
 def encode_pickle(path):
     with open(path, 'rb') as f:
         data = pickle.load(f)
-        
+
         bottleneck_id = np.argmax([v for _, v in data.data])
         lines = [line for line, _ in data.data]
-        
+
         def get_line_range(line_id):
             line_id = min(line_id, len(lines) - 1)
-            lrange = (0, 0)
+            lrange = [0, 0]
             for line in lines[:line_id]:
                 lrange[0] += len(line)
+            lrange[0] += line_id  # newline.
             lrange[1] = lrange[0] + len(lines[line_id])
-            return lrange
+            return tuple(lrange)
 
         ########################################################################
         # lines[bottleneck_id] is unique in the file!
@@ -43,41 +61,60 @@ def encode_pickle(path):
         #     if line_start == 0 or lines[line_start - 1] == lines[bottleneck_id]:
         #         break
         #     line_start -= 1
-        
+
         # line_end = min(len(lines) - 1, bottleneck_id + 1)
         # for _ in range(128):
         #     if line_end == len(lines) - 1 or lines[line_end] == lines[bottleneck_id]:
         #         break
         #     line_end += 1
         ########################################################################
-        
-        line_start = max(0, bottleneck_id - 128)
-        line_end = min(len(lines) - 1, bottleneck_id + 128)
+
+        # fake randomness.
+        line_start = max(0, bottleneck_id - bottleneck_id % 128)
+        line_end = min(len(lines), line_start + 256)
 
         original_bottleneck_range = get_line_range(bottleneck_id)
-        lower_lim = get_line_range(bottleneck_id - 128)[0]
-        upper_lim = get_line_range(bottleneck_id + 128)[1]
+        lower_lim = get_line_range(line_start)[0]
         offset_bottleneck_range = (
-            original_bottleneck_range[0] - lower_lim, 
-            original_bottleneck_range[1] - upper_lim)
+            original_bottleneck_range[0] - lower_lim,
+            original_bottleneck_range[1] - lower_lim)
 
-        cache = __G_LONG_TOKENIZER__.encode_plus(
+        cache = __G_LONG_TOKENIZER__.encode(
             '\n'.join(lines[line_start:line_end]))
 
-        if cache.offsets[-1] >= offset_bottleneck_range[1]:
-            return BatchEncoding(cache.truncate(512)), bottleneck_id - line_start
-        else:
-            bottleneck_token_id_end = None
-            for i, offset in enumerate(cache.offsets):
-                if offset[1] >= offset_bottleneck_range[1]:
-                    bottleneck_token_id_end = i
-                    break
+        bottleneck_token_id_start = None
+        bottleneck_token_id_end = None
+        for i, offset in enumerate(cache.offsets):
+            if bottleneck_token_id_start is None and offset[1] >= offset_bottleneck_range[0]:
+                bottleneck_token_id_start = i
+            if offset[1] >= offset_bottleneck_range[1]:
+                bottleneck_token_id_end = i
+                break
+
+        assert bottleneck_token_id_start is not None, f"line {bottleneck_id + 1} in {path.replace('.pkl', '.txt').replace('pickle/', 'text/')}"
+        assert bottleneck_token_id_end is not None, f"line {bottleneck_id + 1} in {path.replace('.pkl', '.txt').replace('pickle/', 'text/')}"
+        # assert len(cache.ids) >= 512, f"token list too short {len(cache.ids)}"
+
+        if len(cache.ids) < 512 or cache.offsets[511][0] >= offset_bottleneck_range[1]:
+            cache.truncate(512)
+            data = torch.tensor(cache.ids)
+            labels = torch.zeros_like(data, dtype=torch.float)
+            labels[torch.arange(bottleneck_token_id_start,
+                                bottleneck_token_id_end)] = 1.
+            return padding(data, labels)
+        else:  # top 512 context does not contain the bottleneck tokens.
             cache_valid_start = max(0, bottleneck_token_id_end - 512)
-            return BatchEncoding(Encoding(cache.ids[cache_valid_start:(cache_valid_start + 512)])), bottleneck_id - line_start
+            data = torch.tensor(
+                cache.ids[cache_valid_start:(cache_valid_start + 512)])
+            labels = torch.zeros_like(data, dtype=torch.float)
+            labels[torch.arange(bottleneck_token_id_start - cache_valid_start,
+                                bottleneck_token_id_end - cache_valid_start)] = 1.
+            return padding(data, labels)
 
 
 def encode_text(path):
     return encode_string(Path(path).read_text())
+
 
 def tokenize_them_texts(file_list):
     ret = []
@@ -85,8 +122,57 @@ def tokenize_them_texts(file_list):
         ret.append(encode_text(path))
     return ret
 
+
+def tokenize_them_pickles(file_list):
+    ret = []
+    for path in file_list:
+        ret.append(encode_pickle(path))
+    return ret
+
+
+class X86PerfDownstreamDataset(Dataset):
+    def __init__(self, evaluate: bool = False, train_percent=0.9):
+        # Create the tokenizer from a trained one
+        self.examples = []
+
+        files = [str(x) for x in Path("./DATASET/pickle/").glob("**/*.pkl")]
+        n_eval = int(len(files) * (1 - train_percent))
+        if evaluate:
+            files = files[:n_eval]
+        else:
+            files = files[n_eval:]
+
+        # files = files[:1000]  # Debug.
+
+        t0 = time.time()
+        n_process = max(1, min(len(files) // 128, cpu_count() - 1))
+        if n_process <= 1:
+            print('... loading dataset :: [single-threads]')
+            for path in files:
+                self.examples.append(encode_pickle(path))
+        else:
+            print(f'... loading dataset :: [{n_process}-threads]')
+            size = (len(files) + n_process - 1) // n_process
+            with Pool(n_process) as p:
+                task_list = []
+                for i in range(n_process):
+                    task_list.append(files[i * size:(i + 1) * size])
+                for v in p.map(tokenize_them_pickles, task_list):
+                    self.examples += v
+        data_loading_time = time.time() - t0
+        print(
+            f'... finished in {data_loading_time}s --- {len(files) / data_loading_time} files / s')
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, i):
+        # padding here.
+        return self.examples[i]
+
+
 class X86PerfFineTuneDataset(Dataset):
-    def __init__(self, evaluate: bool = False, train_percent = 0.9, pretraining = True): # TODO: Pretraining...
+    def __init__(self, evaluate: bool = False, train_percent=0.9):
         # Create the tokenizer from a trained one
         self.examples = []
 
@@ -97,7 +183,6 @@ class X86PerfFineTuneDataset(Dataset):
         else:
             files = files[n_eval:]
 
-        
         t0 = time.time()
 
         n_process = max(1, min(len(files) // 128, cpu_count() - 1))
@@ -111,11 +196,13 @@ class X86PerfFineTuneDataset(Dataset):
             with Pool(n_process) as p:
                 task_list = []
                 for i in range(n_process):
-                    task_list.append(files[i*size:(i+1)*size])
+                    task_list.append(files[i * size:(i + 1) * size])
                 for v in p.map(tokenize_them_texts, task_list):
                     self.examples += v
         data_loading_time = time.time() - t0
-        print(f'... finished in {data_loading_time}s --- {len(files) / data_loading_time} files / s')
+
+        print(
+            f'... finished in {data_loading_time}s --- {len(files) / data_loading_time} files / s')
 
     def __len__(self):
         return len(self.examples)
--- neuroprof/downstream.py
+++ neuroprof/downstream.py
@@ -1,30 +1,111 @@
-from transformers import RobertaConfig
+from neuroprof.dataset import X86PerfDownstreamDataset
+from neuroprof.data_gen import PerfData
+
+import os
+from datetime import datetime
+
 from transformers import RobertaForMaskedLM
-from neuroprof.dataset import X86PerfFineTuneDataset
+from rich.progress import Progress
+
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader
+from torch import nn
+import torch
 
 if __name__ == '__main__':
     import argparse
-    # args
     parser = argparse.ArgumentParser(description='Downstream')
-    parser.add_argument('--model', type=str, default='./pretrained_model/checkpoint-7552', help='checkpoint dir')
+    parser.add_argument(
+        '--model', type=str, default='./pretrained_model/checkpoint-7552', help='checkpoint dir')
+    parser.add_argument('--epoch', type=int, default=8, help='epoch')
+    parser.add_argument('--lr', type=float, default=1e-5, help='epoch')
+    parser.add_argument('--save_dir', type=str, default='downstream_model',
+                        help='path to save the downstream model')
+    parser.add_argument('--batch_size', type=int,
+                        default=16, help='batch size')
+    parser.add_argument('--log_freq', type=int,
+                        default=200, help='sample size')
     args = parser.parse_args()
 
-    # Set a configuration for our RoBERTa model
-    config = RobertaConfig(
-        vocab_size=2048,
-        max_position_embeddings=514,
-        num_attention_heads=12,
-        num_hidden_layers=6,
-        type_vocab_size=1,
-    )
-    # Initialize the model from a configuration without pretrained weights
-    model = RobertaForMaskedLM.from_pretrained(args.model)
-    print('Num parameters: ', model.num_parameters())
+    model = RobertaForMaskedLM.from_pretrained(args.model).cuda()
+    model.train()
 
     from transformers import RobertaTokenizerFast
     # Create the tokenizer from a trained one
     tokenizer = RobertaTokenizerFast(
         tokenizer_file='./byte-level-bpe.tokenizer.json')
 
-    train_dataset = X86PerfFineTuneDataset(evaluate=False, pretraining=False)
-    eval_dataset = X86PerfFineTuneDataset(evaluate=True, pretraining=False)
+    train_dataset = X86PerfDownstreamDataset(evaluate=False)
+    eval_dataset = X86PerfDownstreamDataset(evaluate=True)
+
+    train_loader = DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=True)
+    eval_loader = DataLoader(eval_dataset, batch_size=32, shuffle=False)
+
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=args.lr, weight_decay=1e-5)
+
+    writer = SummaryWriter(log_dir=os.path.join(
+        args.save_dir, 'runs', datetime.now().strftime('%b%d-%H-%M')))
+
+    with Progress() as progress:
+        training_task = progress.add_task(
+            "[red]Training...", total=args.epoch * len(train_loader))
+        for n_epoch in range(args.epoch):
+            running_loss = 0.0
+            for i_batch, data_batch in enumerate(train_loader):
+                input_ids, attention_mask, labels = data_batch
+                input_ids = input_ids.cuda()
+                attention_mask = attention_mask.cuda()
+                labels = labels.cuda()
+
+                optimizer.zero_grad()
+                loss = loss_fn(model(input_ids=input_ids,
+                                     attention_mask=attention_mask).logits.mean(2), labels)
+                loss.backward()
+                optimizer.step()
+                # print statistics
+                running_loss += loss.item()
+
+                progress.update(training_task, advance=1)
+
+                if i_batch % args.log_freq == args.log_freq - 1:    # print every 2000 mini-batches
+                    n_progress = len(train_loader) * n_epoch + i_batch
+                    writer.add_scalar(
+                        'Loss/train', running_loss / args.log_freq, n_progress)
+                    with torch.no_grad():
+                        eval_running_loss = 0.0
+                        distance_err = 0
+                        n_accurate = 0
+                        n_total = 0
+                        for i_eval, data_eval in enumerate(eval_loader):
+                            input_ids, attention_mask, labels = data_eval
+                            input_ids = input_ids.cuda()
+                            attention_mask = attention_mask.cuda()
+                            labels = labels.cuda()
+
+                            out = model(input_ids=input_ids,
+                                        attention_mask=attention_mask).logits.mean(2)
+                            loss = loss_fn(out, labels)
+
+                            eval_running_loss += loss.item()
+
+                            pred_bottleneck = torch.argmax(out, dim=1)
+                            label_bottleneck = torch.argmax(labels, dim=1)
+                            distance_err += (pred_bottleneck -
+                                             label_bottleneck).abs().sum().item()
+                            n_accurate += (pred_bottleneck ==
+                                           label_bottleneck).sum().item()
+                            n_total += labels.shape[0]
+
+                        writer.add_scalar(
+                            'Loss/eval', eval_running_loss / len(eval_loader), n_progress)
+                        writer.add_scalar(
+                            'Accuracy/eval', n_accurate / n_total, n_progress)
+                        writer.add_scalar(
+                            'AvgTokenDistErr/eval', distance_err / n_total, n_progress)
+                    running_loss = 0.0
+
+    model.save_pretrained(
+        os.path.join(args.save_dir, f'checkpoint-{n_epoch * len(train_loader)}'))
